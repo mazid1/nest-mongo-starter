38d5fe669876c71a4d569c6eebc9da047b46ee12
Mazedul Islam <mazidmailbox@gmail.com>
2023-12-31 11:29:52 +0600

    chore: mongodb connection added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EnvVariables } from './config/env-variables';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      validationSchema: Joi.object({
+        MONGO_CONNECTION_STRING: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService<EnvVariables>) => ({
+        uri: config.get<string>('MONGO_CONNECTION_STRING'),
+      }),
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/config/env-variables.ts
@@ -0,0 +1,3 @@
+export type EnvVariables = {
+  MONGO_CONNECTION_STRING: string;
+};
