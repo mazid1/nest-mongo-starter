fbc8dc62913db920382a8af96be2eb7e6388bdd5
Mazedul Islam <mazidmailbox@gmail.com>
2024-01-04 22:47:51 +0600

    fix: createUser dto validation bug fixed

--- src/modules/users/dtos/createUser.dto.ts
+++ src/modules/users/dtos/createUser.dto.ts
@@ -1,18 +1,30 @@
-import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @IsNotEmpty()
   name: string;
 
   @IsEmail()
   email: string;
 
-  @IsStrongPassword({
-    minLength: 8,
-    minLowercase: 1,
-    minUppercase: 1,
-    minNumbers: 1,
-    minSymbols: 1,
-  })
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'Password must be at least 8 characters long, and contain at least one lowercase letter, one uppercase letter, one number and one special character.',
+    },
+  )
   password: string;
 }
